98407eff634ef7652793015e0e22fd0d48eb0c5c
BingShawLim <shaw.a.muvee@gmail.com>
2020-05-11 13:23:13 -0500

    update About page with personal branding.

--- src/components/About.js
+++ src/components/About.js
@@ -13,11 +13,15 @@ function About(props) {
             <Content>
             <div className="container">
                 <div>
-                <p>My name is Bing Shaw Lim, I am a frontend web developer with experience in Javascript, NodeJS, ReactJS, Redux, JQuery and Ruby on Rails.</p>
-                <p>I am always love about Technology stuffs like smartphone, Machine Learning, Applications and Video Games</p>
-                <p>I'm constantly learning new skills, currently keep practicing with more React projects.</p>
-                <p>My native language is Mandarin, able to understand Cantonese, can read and write/type Simplified and Traditional Chinese‎.</p>
-                {/* <p>I working on passion projects about Cooking & Graphic design.</p> */}
+                    <h3>Front-End Web Developer with Graphic Design & native level Chinese language skills.</h3>
+
+                    <p>Having developed skills in HTML5, CSS3, Bootstrap, JavaScript, ReactJS, Redux, JQuery, Ruby, Ruby on Rails, Firebase and Postgresql.</p>
+
+                    <p>I am constantly learning new skills, currently enhancing my ReactJS knowledge by building different projects, and gaining backend database technology.</p>
+
+                    <p>Known as a quick learner and good resource to ask the right questions, to get the right solution for the problems. Passionate about developing web applications and responsive websites.</p>
+
+                    <p>A team-oriented player with the ability to grow myself and my team.  My desire is a position with a company that values my skill set and that looks for individuals who can think outside the box, for solutions to real-world problems, evolving and growing with a diverse team. </p>
                 </div>
                 <div className="my-profile">
                 <img className="my-profile-image" src={meimg} alt={meimg}/>
@@ -29,3 +33,10 @@ function About(props) {
 }
 
 export default About;
+
+
+                {/* <p>My name is Bing Shaw Lim, I am a frontend web developer with experience in Javascript, NodeJS, ReactJS, Redux, JQuery and Ruby on Rails.</p>
+                <p>I am always love about Technology stuffs like smartphone, Machine Learning, Applications and Video Games</p>
+                <p>I'm constantly learning new skills, currently keep practicing with more React projects.</p>
+                <p>My native language is Mandarin, able to understand Cantonese, can read and write/type Simplified and Traditional Chinese‎.</p> */}
+{/* <p>I working on passion projects about Cooking & Graphic design.</p> */}
